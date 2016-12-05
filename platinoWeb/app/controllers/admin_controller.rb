5e618dd87986ac08d44f158075b714b34480b20d
santiago <santivaz852@hotmail.com>
2016-12-05 15:18:33 -0500

    en procedo de autenticacion

--- platinoWeb/app/controllers/admin_controller.rb
+++ platinoWeb/app/controllers/admin_controller.rb
@@ -1,2 +1,65 @@
 class AdminController < ApplicationController
+    include ApplicationHelper
+  
+  def autenticar
+    parameters = login_params
+    puts parameters
+    if user = Usuario.find_by(correo: parameters[:inputEmail])
+      if user.clave == parameters[:inputPassword]
+        session[:user_id] = user.id
+         redirect_to admin_inicio_url
+      else
+        flash[:error] = "Clave Incorrecta"
+        redirect_to admin_login_url
+      end
+
+    else
+      flash[:error] = "Datos incorrectos"
+      redirect_to admin_login_url
+    end
+  end
+ 
+    def login
+    end
+
+  def cerrar_sesion
+    session.delete(:user_id)
+    redirect_to admin_login_url
+  end
+    
+    def inicio
+       
+    end
+    
+    
+
+  def suscripcion
+    NotificacionesMailer.suscribir(suscripcion_params[:email]).deliver_now
+    flash[:success] = "Suscripción completa"
+    redirect_to root_url
+  end
+  def contacto
+    parameters = contacto_params
+    nombre = parameters[:nombre]
+    correo = parameters[:correo]
+    tema = parameters[:tema]
+    mensaje = parameters[:mensaje]
+    NotificacionesMailer.contacto(correo,nombre,tema,mensaje).deliver_now
+    flash[:success] = "Gracias, nos estaremos comunicando con usted"
+    redirect_to store_contact_url 
+  end  
+  private 
+  
+  def login_params
+    params.permit(:inputPassword, :inputEmail)
+  end
+
+ 
+  def suscripcion_params
+    params.permit(:email)
+  end
+
+  def contacto_params
+    params.permit(:nombre, :correo, :tema, :mensaje)
+  end
 end
--- platinoWeb/config/routes.rb
+++ platinoWeb/config/routes.rb
@@ -3,5 +3,14 @@ Rails.application.routes.draw do
     root 'welcome#index'
     get 'contacto/contact'
     get 'admin/login'
+    get 'admin/inicio'
+    get 'welcome/catalogo'
+    get 'welcome/diseno'
+    get 'welcome/planta'
+    get 'welcome/lavanderia'
+    get 'welcome/corte'
+    get 'welcome/terminacion'
+    post 'autenticar/' => 'admin#autenticar', as: 'login'
+    delete 'cerrar_sesion/' => 'admin#cerrar_sesion', as: 'cerrar_sesion'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
